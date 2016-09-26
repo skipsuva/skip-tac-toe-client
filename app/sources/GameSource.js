36e8d59cde7b35ad2a1d09d089a9ea5e037a4415
skipsuva <skip.suva@gmail.com>
2016-09-26 15:42:09 -0400

    change url for AJAX

--- app/sources/GameSource.js
+++ app/sources/GameSource.js
@@ -7,7 +7,7 @@ var GameSource = {
   createGame(playerInitials) {
     return new Promise(function (resolve, reject) {
       $.ajax({
-        url: 'http://localhost:3000/v1/create',
+        url: 'https://skip-tac-toe-api.herokuapp.com/v1/create',
         method: 'POST',
         data: { player_initials: playerInitials }
       }).done(resolve).fail(reject);
