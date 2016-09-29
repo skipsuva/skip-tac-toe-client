c779977d74f47d6ef276e3fa12d34a2f20c93ce1
skipsuva <skip.suva@gmail.com>
2016-09-29 15:54:06 -0400

    add pngs to loader

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,8 @@ module.exports = {
       {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader",
         query: {presets: ['es2015', 'react']}
       },
-      {test: /\.scss$/, loaders: ['style', 'css', 'sass']}
+      {test: /\.scss$/, loaders: ['style', 'css', 'sass']},
+      {test: /\.png$/, loader: 'file'}
     ]
   },
   plugins: [
